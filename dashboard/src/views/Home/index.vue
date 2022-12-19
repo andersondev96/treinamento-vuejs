2b44527331d60e260c1dfb25ba584f2ac3c1d82d
Anderson Fernandes <anderson@lumiun.com>
2022-12-19 08:43:21 -0300

    Criando a tela inicial (Parte 1)

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1 class="text-brand-main font-black">Aqui é a home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2022</p>
+  </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
