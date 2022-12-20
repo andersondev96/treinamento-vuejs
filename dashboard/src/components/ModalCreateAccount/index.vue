95742f5231e245418c88d9dad899a64f9ec7bba7
Anderson Fernandes <anderson@lumiun.com>
2022-12-20 09:24:16 -0300

    Criando nosso modal factory (Parte 2)

---
+++ dashboard/src/components/ModalCreateAccount/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Modal de criação de conta</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ dashboard/src/components/ModalLogin/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Modal de login</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
