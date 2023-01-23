f74f6c4cc0bfb9be418068841905ffcd6b86fc9d
Anderson Fernandes <anderson@lumiun.com>
2023-01-23 08:23:03 -0300

    Setup dos testes unitários com componentes TypeScript

---
+++ widget/src/views/Playground/Playground.spec.js
@@ -0,0 +1,10 @@
+
+import { shallowMount } from '@vue/test-utils'
+import Playground from './index.vue'
+
+describe('<Playground />', () => {
+  it('should component render correctly', () => {
+    const wrapper = shallowMount(Playground)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
